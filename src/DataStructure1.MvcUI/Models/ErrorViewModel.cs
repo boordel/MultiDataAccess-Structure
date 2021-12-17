2d251aa82d1332830c8e927587d0a96cf8c182ff
Mahmood <boordel@outlook.com>
2021-12-17 12:40:51 +0330

    This is not needed

--- src/DataStructure1.MvcUI/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace DataStructure1.MvcUI.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
